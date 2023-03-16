c08e846d7beee7d616fbd5b5eec4fa44a771d5e0
hwxiiao <hwxiao@baai.ac.cn>
2023-03-16 19:47:49 +0800

    update

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-batch_size=16
+batch_size=$1
 model_size=large
 model=vit_${model_size}_patch16
-tar=CXC
-split_ratio=8:1:1
+tar=$2
+split_ratio=$3
 # finetune=/home/hwxiao/medical_pretrained_models/MAE/mae_pretrain_vit_${model_size}.pth
 # finetune=/home/hwxiao/medical_pretrained_models/MAE/data13_mae_pretrain_vit_${model_size}.pth
 # finetune=/home/hwxiao/medical_pretrained_models/MAE/data14_mae_pretrain_vit_${model_size}.pth
 # finetune=/home/hwxiao/medical_pretrained_models/MAE/data21_mae_pretrain_vit_${model_size}.pth
-finetune=/home/hwxiao/medical_pretrained_models/MAE/data36_mae_pretrain_vit_${model_size}.pth
+# finetune=/home/hwxiao/medical_pretrained_models/MAE/data36_mae_pretrain_vit_${model_size}.pth
 # finetune=/home/hwxiao/medical_pretrained_models/MAE/allCT_mae_pretrain_vit_${model_size}.pth
 # finetune=/home/hwxiao/medical_pretrained_models/MAE/CXC_mae_pretrain_vit_${model_size}.pth
 
 # finetune=/home/hwxiao/medical_pretrained_models/inter_model/inter_CXC_8:1:1_mae_pretrain_vit_${model_size}.pth
 # finetune=/home/hwxiao/medical_pretrained_models/inter_model/inter_CXC_8:1:1_data13_mae_vit_${model_size}.pth
-# finetune=/home/hwxiao/medical_pretrained_models/inter_model/inter_CXC_8:1:1_data36_mae_vit_${model_size}.pth
+finetune=/home/hwxiao/medical_pretrained_models/inter_model/inter_CXC_8:1:1_data36_mae_vit_${model_size}.pth
 
 # finetune=/home/hwxiao/medical_pretrained_models/TFS_model/CXC_8:1:1_vit_b_16.pth
 # finetune=/home/hwxiao/medical_pretrained_models/TFS_model/CXC_8:1:1_vit_l_16.pth
 
-OMP_NUM_THREADS=1 torchrun --nnodes=1 --nproc_per_node=2 main_finetune.py \
+OMP_NUM_THREADS=1 torchrun --nnodes=1 --nproc_per_node=$4 main_finetune.py \
         --model ${model} \
         --save_all \
         --batch_size ${batch_size} \
@@ -28,9 +28,10 @@ OMP_NUM_THREADS=1 torchrun --nnodes=1 --nproc_per_node=2 main_finetune.py \
         --split_ratio ${split_ratio} \
         --epochs 200 \
         --finetune ${finetune} \
-        --ckpt_save_freq 10 \
-        --blr 0.001 --layer_decay 0.75 --weight_decay 0.05 --drop_path 0.1 --reprob 0.25 --mixup 0.8 --cutmix 1.0
-        # --blr 0.0029628906306211466 --cutmix 0.2463813127293306 --drop_path 0.21969988402746932 --layer_decay 0.7373740569816005 --mixup 0.6614802880227085 --reprob 0.3167303518852447  --weight_decay 0.06957806900582497
+        --ckpt_save_freq 1 \
+        --blr 0.0029628906306211466 --cutmix 0.2463813127293306 --drop_path 0.21969988402746932 --layer_decay 0.7373740569816005 --mixup 0.6614802880227085 --reprob 0.3167303518852447  --weight_decay 0.06957806900582497
+        # --blr 0.001 --layer_decay 0.75 --weight_decay 0.05 --drop_path 0.1 --reprob 0.25 --mixup 0.8 --cutmix 1.0 \
+        # --resume /home/hwxiao/mycodes/MAE_COVID19/outputs/output_finetune/finetune_with_data36_mae_pretrain_vit_large/8:1:1/CXC_seed42_bs64_b0.001_l0.75_w0.05_d0.1_r0.25_m0.8_c1.0_rb0_fb0_attnFalse_mlpFalse_biasFalse_FFT/checkpoint-74.pth
   
 # base    --blr 0.0005 --layer_decay 0.65 --weight_decay 0.05 --drop_path 0.1 --reprob 0.25 --mixup 0.8 --cutmix 1.0
 
