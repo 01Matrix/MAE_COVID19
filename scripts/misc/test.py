ef27901e17b2d1dfd20bc2dfb01d0231e0e98b84
hwxiao <hwxiao@gitee.com>
2024-09-20 21:47:25 +0800

    update

--- scripts/misc/test.py
+++
@@ -1,6 +0,0 @@
-import sys, os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-print(os.path.dirname(os.path.abspath(__file__)))
-from . import main_pretrain as trainer
-
-print('success!')
